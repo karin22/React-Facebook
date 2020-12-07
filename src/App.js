49eeddc1dc2667bea02e007ea16ad5fd3dff3810
Karin <not.karin@hotmail.com>
2020-12-08 00:28:06 +0700

    commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './App.css';
+
+import Navbar from './Navber'
+import Input from './Input'
+import Post from './Post'
+
+let id = 1;
+
+function App() {
+  const [posts, setPosts] = useState([]);
+
+  function addPost(title) {
+    const newPost = { id, title } // id= id << title = title
+    setPosts([newPost, ...posts]); //เก็บ newPost และ postเก่า
+    id += 1;
+  }
+
+  function deletePost(id){
+    const updatedPosts = posts.filter((post) => post.id != id);
+    setPosts(updatedPosts)
+  }
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Input addPost={addPost} />
+      {posts.map((post) => (
+      <Post key={post.id} id={post.id} title={post.title} deletePost={deletePost}/>
+      ))}
+
+    </div>
+  );
+}
+
+export default App;
